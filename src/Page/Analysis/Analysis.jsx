1deb96dcb4976116053f52cf84845fdf75aa611c
rayhanalmim <rayhanalmim1@gmail.com>
2024-02-24 12:55:42 +0600

    fetch all sell data from db

--- src/Page/Analysis/Analysis.jsx
+++ src/Page/Analysis/Analysis.jsx
@@ -1,18 +1,20 @@
+import { useQuery } from "@tanstack/react-query";
+import useAxiosSecure from "../../Hook/useAxiosSecure";
 
 const Analysis = () => {
 
-    const sellData = [
-        {
-            "_id": "65d372a56e2bc190b3d39558",
-            "totalSellAmmount": 70215,
-            "monthlySellAmount": [{ "month": "2024-02", "totalAmmount": 70215 }],
-            "yearlySellAmount": [{ "year": "2024", "totalAmount": 70215 }],
-            "companyInfo": "adminCollection",
-            "dailySellAmmount": [{ "day": "2024-02-23", "totalAmmount": "0" }, { "day": "2024-02-24", "totalAmmount": 3300 }]
-        }
-    ];
+    const axiosSecure = useAxiosSecure();
+  const { data: sellData = [] } = useQuery({
+    queryKey: ["sellInfo"],
+    queryFn: async () => {
+      const res = await axiosSecure.get("/sell/allSell");
+      return res.data;
+    },
+  });
 
-    const { totalSellAmmount, monthlySellAmount, yearlySellAmount, dailySellAmmount } = sellData[0];
+
+    const { totalSellAmmount, monthlySellAmount, yearlySellAmount, dailySellAmmount } = sellData;
+
 
     const formatMonth = (monthString) => {
         const date = new Date(monthString);
@@ -48,7 +50,7 @@ const Analysis = () => {
                 <h3 className="text-lg font-semibold m-2">Daily Sell Amount:</h3>
                 <table className="table-auto border-collapse w-full">
                     <tbody>
-                        {dailySellAmmount.map(dayData => (
+                        {dailySellAmmount?.map(dayData => (
                             <tr key={dayData.day}>
                                 <td className="border px-4 py-2">{formatDateForDaily(dayData.day)}</td>
                                 <td className="border px-4 py-2">{dayData.totalAmmount}</td>
@@ -62,7 +64,7 @@ const Analysis = () => {
                 <h3 className="text-lg font-semibold mb-2">Monthly Sell Amount:</h3>
                 <table className="table-auto border-collapse w-full">
                     <tbody>
-                        {monthlySellAmount.map(monthData => (
+                        {monthlySellAmount?.map(monthData => (
                             <tr key={monthData.month}>
                                 <td className="border px-4 py-2">{formatMonth(monthData.month)}</td>
                                 <td className="border px-4 py-2">{monthData.totalAmmount}</td>
@@ -76,7 +78,7 @@ const Analysis = () => {
                 <h3 className="text-lg font-semibold mb-2">Yearly Sell Amount:</h3>
                 <table className="table-auto border-collapse w-full">
                     <tbody>
-                        {yearlySellAmount.map(yearData => (
+                        {yearlySellAmount?.map(yearData => (
                             <tr key={yearData.year}>
                                 <td className="border px-4 py-2">{yearData.year}</td>
                                 <td className="border px-4 py-2">{yearData.totalAmount}</td>
